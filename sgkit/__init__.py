54c66e08e552b5e4666addb56873019fe2f32914
Rafal Wojdyla <ravwojdyla@gmail.com>
2020-10-28 16:14:41 +0100

    Try one more

--- sgkit/__init__.py
+++ sgkit/__init__.py
@@ -13,33 +13,10 @@ from .stats.association import gwas_linear_regression
 from .stats.hwe import hardy_weinberg_test
 from .stats.pc_relate import pc_relate
 from .stats.pca import pca
-from .stats.popgen import Fst, Tajimas_D, divergence, diversity
+from .stats.popgen import Fst as Fst
+from .stats.popgen import Tajimas_D as Tajimas_D
+from .stats.popgen import divergence as divergence
+from .stats.popgen import diversity as diversity
 from .stats.preprocessing import filter_partial_calls
 from .stats.regenie import regenie
 from .testing import simulate_genotype_call_dataset
-
-__all__ = [
-    "DIM_ALLELE",
-    "DIM_PLOIDY",
-    "DIM_SAMPLE",
-    "DIM_VARIANT",
-    "create_genotype_call_dataset",
-    "count_variant_alleles",
-    "count_call_alleles",
-    "create_genotype_dosage_dataset",
-    "display_genotypes",
-    "filter_partial_calls",
-    "gwas_linear_regression",
-    "read_vcfzarr",
-    "regenie",
-    "hardy_weinberg_test",
-    "variant_stats",
-    "diversity",
-    "divergence",
-    "Fst",
-    "Tajimas_D",
-    "pc_relate",
-    "simulate_genotype_call_dataset",
-    "variables",
-    "pca",
-]
